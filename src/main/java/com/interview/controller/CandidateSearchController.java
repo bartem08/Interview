8378248d9f480f018b648fc347bdfb48745ba1ed
Artem <Artem@localhost>
2016-11-15 00:12:16 +0200

    Candidate search implementation

--- src/main/java/com/interview/controller/CandidateSearchController.java
+++ src/main/java/com/interview/controller/CandidateSearchController.java
@@ -2,10 +2,14 @@ package com.interview.controller;
 
 import com.interview.model.entity.Direction;
 import com.interview.model.projection.Title;
+import com.interview.repository.CandidateSearchRepository;
 import com.interview.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -17,20 +21,33 @@ public class CandidateSearchController extends AbstractController {
 
     private DirectionService directionService;
 
+    private CandidateSearchRepository candidateSearchRepository;
+
     @Autowired
     public CandidateSearchController(MessageService messageService, CandidateService candidateService,
+                                     CandidateSearchRepository candidateSearchRepository,
                                      DirectionService directionService) {
         super(messageService);
+        this.candidateSearchRepository = candidateSearchRepository;
         this.candidateService = candidateService;
         this.directionService = directionService;
     }
 
-    @RequestMapping(produces = APPLICATION_JSON_VALUE)
-    public Page<Title> findByDirections(@RequestParam("direction") String directionID, Pageable pageable) {
+    @RequestMapping(value = "/direction", produces = APPLICATION_JSON_VALUE)
+    public Page<Title> findByDirections(@RequestParam("id") String directionID, Pageable pageable) {
 
         Direction direction = directionService.getNotNull(directionID, "direction.label");
 
         return candidateService.findByDirections(pageable, direction)
                 .map(this::buildTitle);
+    }
+
+    @RequestMapping(value = "/name", produces = APPLICATION_JSON_VALUE)
+    public List<Title> findByName(String term, Integer offset) {
+
+        return candidateSearchRepository.findCandidates(term, offset)
+                .stream()
+                .map(this::buildTitle)
+                .collect(Collectors.toList());
     }
 }
