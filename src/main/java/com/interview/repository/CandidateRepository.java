99fd9226e9d9df51c4ca9af3ed6ade5ffe3da9dd
Artem <Artem@localhost>
2016-11-15 22:03:27 +0200

    Keep search by name or phone

--- src/main/java/com/interview/repository/CandidateRepository.java
+++ src/main/java/com/interview/repository/CandidateRepository.java
@@ -1,15 +1,7 @@
 package com.interview.repository;
 
 import com.interview.model.entity.Candidate;
-import com.interview.model.entity.Direction;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.util.Collection;
-
 @Repository
-public interface CandidateRepository extends AbstractRepository<Candidate, String> {
-
-    Page<Candidate> findAllByDirectionsIn(Collection<Direction> directions, Pageable pageable);
-}
+public interface CandidateRepository extends AbstractRepository<Candidate, String> {}
